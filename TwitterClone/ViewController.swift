c613e4f13ccbec78966aa1ef4310bab3ae559aca
Mehmet Ali Er <mehmetalier83@hotmail.com>
2024-01-15 22:22:24 +0300

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Mehmet Ali Er on 15.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
