852cecd2974f9e7d0b961b4f1ecd82291d811bc8
Mehmet Ali Er <mehmetalier83@hotmail.com>
2024-01-16 23:42:28 +0300

    ProfileTableViewHeader yazıldı.

--- TwitterClone/Controllers/HomeViewController.swift
+++ TwitterClone/Controllers/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     private let timelineTableView : UITableView = {
         let tableView = UITableView()
         tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
@@ -21,10 +21,29 @@ class HomeViewController: UIViewController {
         view.addSubview(timelineTableView)
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
+        
+        configureNavigationBar()
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         timelineTableView.frame = view.bounds
+    }
+    private func configureNavigationBar(){
+        let size: CGFloat = 24
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFit
+        logoImageView.image = UIImage(named:"x")
+        
+        let middleView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        middleView.addSubview(logoImageView)
+        navigationItem.titleView = middleView
+        
+        let profileImageView = UIImage(systemName: "person")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImageView, style: .plain, target: self, action: #selector(didTapProfile))
+    }
+    @objc private func didTapProfile(){
+        let vc = ProfileViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
